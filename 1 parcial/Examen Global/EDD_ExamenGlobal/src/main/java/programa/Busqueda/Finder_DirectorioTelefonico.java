20cae6bb5125f4c7e4b7311576a11538fd1a70c8
Bryant <bryantgamboajimenez@gmail.com>
2022-08-17 01:42:56 -0500

    Agregando cambios

--- 1 parcial/Examen Global/EDD_ExamenGlobal/src/main/java/programa/Busqueda/Finder_DirectorioTelefonico.java
+++ 1 parcial/Examen Global/EDD_ExamenGlobal/src/main/java/programa/Busqueda/Finder_DirectorioTelefonico.java
@@ -1,6 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * 1. Se cuenta con la información del directorio telefónico almacenada en una matriz de 3 columnas, 
+en la primera está el apellido, en la segunda el nombre y en la tercera el número telefónico. 
+Se requiere un algoritmo para buscar el número de teléfono de una persona tomando como clave su apellido y nombre.
  */
 package programa.Busqueda;
 
