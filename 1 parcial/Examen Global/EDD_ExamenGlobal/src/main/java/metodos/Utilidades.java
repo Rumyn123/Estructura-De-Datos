58fcd38ecee5ca9ced456bfd94be39f475162dbe
Bryant <bryantgamboajimenez@gmail.com>
2022-08-16 21:53:59 -0500

    Se agregan clases para trabajar

---
+++ 1 parcial/Examen Global/EDD_ExamenGlobal/src/main/java/metodos/Utilidades.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package metodos;
+
+import objetos.*;
+/**
+ *
+ * @author Rumyn
+ */
+public class Utilidades {
+    
+}
