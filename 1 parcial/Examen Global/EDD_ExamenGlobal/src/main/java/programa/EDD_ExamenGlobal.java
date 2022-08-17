6f981000734ba862ce474630b383df0ff891c764
Bryant <bryantgamboajimenez@gmail.com>
2022-08-16 22:03:08 -0500

    Me faltaron cambios

--- 1 parcial/Examen Global/EDD_ExamenGlobal/src/main/java/programa/EDD_ExamenGlobal.java
+++ 1 parcial/Examen Global/EDD_ExamenGlobal/src/main/java/programa/EDD_ExamenGlobal.java
@@ -4,6 +4,8 @@
  */
 package programa;
 
+import metodos.Utilidades;
+
 /**
  *
  * @author Rumyn
@@ -14,7 +16,7 @@ public class EDD_ExamenGlobal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Utilidades.msg("prueba");
     }
     
 }
